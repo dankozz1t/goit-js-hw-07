2085a9ee938d6328dff966d73c38eb9d3fa4d657
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-03 15:12:29 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", renderGalleryItems());
+
+function renderGalleryItems() {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoomFactor: false,
+});
