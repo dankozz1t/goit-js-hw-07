f4aec62ac88a407602a2d118f189eb33b39cb9dc
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-03 09:43:55 +0300

    add close on hot key

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,9 +25,25 @@ galleryEl.insertAdjacentHTML("beforeend", renderGalleryItems());
 galleryEl.addEventListener("click", onImageClick);
 
 function onImageClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
   event.preventDefault();
 
   event.target.src = event.target.dataset.source;
 
-  basicLightbox.create(event.target.outerHTML).show();
+  basicLightbox
+    .create(event.target.outerHTML, { onShow: closeOnHotKey })
+    .show();
+
+  function closeOnHotKey(instance) {
+    const onCloseKeyDown = (event) => {
+      if (event.key === "Escape") {
+        instance.close();
+        galleryEl.removeEventListener("keydown", onCloseKeyDown);
+      }
+    };
+
+    galleryEl.addEventListener("keydown", onCloseKeyDown);
+  }
 }
