e849b385342210568277c16ded0fa90d89c80f10
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-03 09:14:17 +0300

    add task 1 photo gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const renderGalleryItems = () =>
+  galleryItems.reduce(
+    (previousValue, { original, preview, description }) =>
+      (previousValue += `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>
+`),
+    ""
+  );
+
+galleryEl.insertAdjacentHTML("beforeend", renderGalleryItems());
+
+const galleryLink = document.querySelector(".gallery");
+
+galleryLink.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  event.target.src = event.target.dataset.source;
+
+  basicLightbox.create(event.target.outerHTML).show();
+}
